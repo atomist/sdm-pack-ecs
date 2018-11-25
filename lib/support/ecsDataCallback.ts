3a2bbc3ee4c8fc6eeef5ae5c96ce21ee9b20d8e7
ipcrm <mcadorette@gmail.com>
2018-11-25 12:08:50 -0500

    wip: allow in-project configs

--- lib/support/ecsDataCallback.ts
+++ lib/support/ecsDataCallback.ts
@@ -1,6 +1,7 @@
-import {logger } from "@atomist/automation-client";
+import {logger, Project } from "@atomist/automation-client";
 import { RepoContext, SdmGoalEvent} from "@atomist/sdm";
 import { ECS } from "aws-sdk";
+import * as path from "path";
 import { createEcsSession } from "../EcsSupport";
 import { EcsDeploy, EcsDeployRegistration } from "../goals/EcsDeploy";
 import { createValidServiceRequest } from "./ecsServiceRequest";
@@ -15,126 +16,196 @@ export function ecsDataCallback(
             credentials: ctx.credentials, id: ctx.id, context: ctx.context, readOnly: true,
         }, async p => {
 
-            // Set image string, example source value:
-            //  <registry>/<author>/<image>:<version>"
-            const imageString = sdmGoal.push.after.image.imageName.split("/").pop().split(":")[0];
+            try {
+                // Merge task definition configurations together - SDM Goal registration and in project
+                //   in-project wins
+                const newTaskDef = await getFinalTaskDefinition(p, sdmGoal, registration);
 
-            // Create or Update a task definition
-            // Check for passed taskdefinition info, and update the container field
-            let newTaskDef: ECS.Types.RegisterTaskDefinitionRequest = {
-                family: "",
-                containerDefinitions: [],
-            };
+                // Populate service request
+                //   Load any in project config and merge with default generated; in project wins
+                const tempServiceRequest = await createValidServiceRequest(registration.serviceRequest);
+                const inProjectSr = await readEcsServiceSpec(p, "service.json");
+                const validServiceRequest = {...tempServiceRequest, ...inProjectSr};
 
-            // If our registration doesn't include a task definition - generate a generic one
-            if (!registration.taskDefinition) {
-                // TODO: Check if there is an in-project configuration
-                let dockerFile;
-                if (p.hasFile("Dockerfile")) {
-                            const d = await p.getFile("Dockerfile");
-                            dockerFile = await d.getContent();
+                // Retrieve existing Task definitions, if we find a matching revision - use that
+                //  otherwise create a new task definition
+                const ecs = createEcsSession(registration.region);
+
+                // Pull latest def info & compare it to the latest
+                let goodTaskDefinition: ECS.Types.TaskDefinition;
+                const taskDefs = await ecsListTaskDefinitions(ecs, newTaskDef.family);
+                let latestRev;
+                await ecsGetTaskDefinition(ecs, taskDefs.pop())
+                    .then(v => {
+                        latestRev = v;
+                    })
+                    .catch(() => {
+                        logger.debug(`No task definitions found for ${newTaskDef.family}`);
+                    });
+
+                // Compare latest def to new def
+                // - if they differ create a new revision
+                // - if they don't use the existing rev
+                if (latestRev && !cmpSuppliedTaskDefinition(latestRev, newTaskDef)) {
+                    goodTaskDefinition = await ecsRegisterTask(ecs, newTaskDef);
+                } else if (!latestRev) {
+                    goodTaskDefinition = await ecsRegisterTask(ecs, newTaskDef);
                 } else {
-                    throw Error("No task definition present and no dockerfile found!");
+                    goodTaskDefinition = latestRev;
                 }
 
-                // Get Docker commands out
-                const parser = require("docker-file-parser");
-                const options = { includeComments: false };
-                const commands = parser.parse(dockerFile, options);
-                const exposeCommands = commands.filter((c: any) => c.name === "EXPOSE");
+                // IF there is a local definition (ie in-project configuration) override the values found here
+                // TODO
 
-                if (exposeCommands.length > 1) {
-                    throw new Error(`Unable to determine port for default ingress. Dockerfile in project ` +
-                        `'${sdmGoal.repo.owner}/${sdmGoal.repo.name}' has more then one EXPOSE instruction: ` +
-                        exposeCommands.map((c: any) => c.args).join(", "));
-                } else if (exposeCommands.length === 1) {
-                    newTaskDef.family = imageString;
-                    // TODO: Expose the defaults below in client.config.json
-                    newTaskDef.requiresCompatibilities = [ "FARGATE"];
-                    newTaskDef.networkMode = "awsvpc";
-                    newTaskDef.cpu = "256",
-                    newTaskDef.memory = "0.5GB",
+                // Update Service Request with up to date task definition
+                let newServiceRequest: ECS.Types.CreateServiceRequest;
+                newServiceRequest = {
+                    ...validServiceRequest,
+                    serviceName: validServiceRequest.serviceName ? validServiceRequest.serviceName : sdmGoal.repo.name,
+                    taskDefinition: `${goodTaskDefinition.family}:${goodTaskDefinition.revision}`,
+                };
 
-                    newTaskDef.containerDefinitions = [
-                        {
-                            name: imageString,
-                            healthCheck: {
-                                command: [
-                                    "CMD-SHELL",
-                                    `wget -O /dev/null http://localhost:${exposeCommands[0].args[0]} || exit 1`,
-                                ],
-                                startPeriod: 30,
-                            },
-                            image: sdmGoal.push.after.image.imageName,
-                            portMappings: [{
-                                containerPort: exposeCommands[0].args[0],
-                                hostPort: exposeCommands[0].args[0],
-                            }],
-                        },
-                    ];
-                }
-            } else {
-                newTaskDef = registration.taskDefinition;
-                newTaskDef.containerDefinitions.forEach( k => {
-                    if (imageString === k.name) {
-                        k.image = sdmGoal.push.after.image.imageName;
-                    }
-                    // TODO: Expose the defaults below in client.config.json
-                    k.memory = k.hasOwnProperty("memory") && k.memory ? k.memory : 1024;
-                    k.cpu = k.hasOwnProperty("cpu") && k.cpu ? k.cpu : 256;
-                });
+                return {
+                    ...sdmGoal,
+                    data: JSON.stringify({
+                        serviceRequest: newServiceRequest,
+                        taskDefinition: goodTaskDefinition,
+                        region: registration.region,
+                    }),
+                };
+            } catch (error) {
+                logger.error(error);
+                throw new Error(error);
             }
-
-            // Populate service request
-            const validServiceRequest = await createValidServiceRequest(registration.serviceRequest);
-
-            // Retrieve existing Task definitions, if we find a matching revision - use that
-            //  otherwise create a new task definition
-            const ecs = createEcsSession(registration.region);
-
-            // Pull latest def info & compare it to the latest
-            let goodTaskDefinition: ECS.Types.TaskDefinition;
-            const taskDefs = await ecsListTaskDefinitions(ecs, newTaskDef.family);
-            let latestRev;
-            await ecsGetTaskDefinition(ecs, taskDefs.pop())
-                .then(v => {
-                    latestRev = v;
-                })
-                .catch(() => {
-                    logger.debug(`No task definitions found for ${newTaskDef.family}`);
-                });
-
-            // Compare latest def to new def
-            // - if they differ create a new revision
-            // - if they don't use the existing rev
-            if (latestRev && !cmpSuppliedTaskDefinition(latestRev, newTaskDef)) {
-                goodTaskDefinition = await ecsRegisterTask(ecs, newTaskDef);
-            } else if (!latestRev) {
-                goodTaskDefinition = await ecsRegisterTask(ecs, newTaskDef);
-            } else {
-                goodTaskDefinition = latestRev;
-            }
-
-            // IF there is a local definition (ie in-project configuration) override the values found here
-            // TODO
-
-            // Update Service Request with up to date task definition
-            let newServiceRequest: ECS.Types.CreateServiceRequest;
-            newServiceRequest = {
-                ...validServiceRequest,
-                serviceName: validServiceRequest.serviceName ? validServiceRequest.serviceName : sdmGoal.repo.name,
-                taskDefinition: `${goodTaskDefinition.family}:${goodTaskDefinition.revision}`,
-            };
-
-            return {
-                ...sdmGoal,
-                data: JSON.stringify({
-                    serviceRequest: newServiceRequest,
-                    taskDefinition: goodTaskDefinition,
-                    region: registration.region,
-                }),
-            };
         });
 
     };
 }
+
+export async function getSpecFile(p: Project, name: string):
+    Promise<JSON | undefined> {
+        return new Promise<JSON | undefined>(async (resolve, reject) => {
+            const specPath = path.join(".atomist", "ecs", name);
+            try {
+                const specFile = await p.getFile(specPath);
+                if (specFile) {
+                    const spec = await specFile.getContent();
+                    resolve(JSON.parse(spec));
+                } else {
+                    resolve(undefined);
+                }
+            } catch (e) {
+                logger.warn(`Failed to read spec file ${specPath}: ${e.message}`);
+                reject(e.message);
+            }
+        });
+    }
+
+export async function readEcsServiceSpec(p: Project, name: string):
+    Promise<Partial<ECS.Types.CreateServiceRequest>> {
+    return new Promise<Partial<ECS.Types.CreateServiceRequest>>(async (resolve, reject) => {
+        resolve(getSpecFile(p, name) as Partial<ECS.Types.CreateServiceRequest>);
+    });
+}
+
+export async function readEcsTaskSpec(p: Project, name: string):
+    Promise<Partial<ECS.Types.RegisterTaskDefinitionRequest>> {
+    return new Promise<Partial<ECS.Types.RegisterTaskDefinitionRequest>>(async (resolve, reject) => {
+        resolve(getSpecFile(p, name) as Partial<ECS.Types.RegisterTaskDefinitionRequest>);
+    });
+}
+
+export async function getFinalTaskDefinition(
+    p: Project,
+    sdmGoal: SdmGoalEvent,
+    registration: EcsDeployRegistration): Promise<ECS.Types.RegisterTaskDefinitionRequest> {
+        return new Promise<ECS.Types.RegisterTaskDefinitionRequest>(async (resolve, reject) => {
+            try {
+                // Set image string, example source value:
+                //  <registry>/<author>/<image>:<version>"
+                const imageString = sdmGoal.push.after.image.imageName.split("/").pop().split(":")[0];
+
+                // Create or Update a task definition
+                // Check for passed taskdefinition info, and update the container field
+                let newTaskDef: ECS.Types.RegisterTaskDefinitionRequest = {
+                    family: "",
+                    containerDefinitions: [],
+                };
+
+                // Check if there is an in-project configuration
+                // .atomist/task-definition.json
+                const inProjectTaskDef = await readEcsTaskSpec(p, "task-definition.json");
+
+                // If our registration doesn't include a task definition - generate a generic one
+                if (!registration.taskDefinition && inProjectTaskDef === undefined) {
+                    let dockerFile;
+                    if (p.hasFile("Dockerfile")) {
+                                const d = await p.getFile("Dockerfile");
+                                dockerFile = await d.getContent();
+                    } else {
+                        throw Error("No task definition present and no dockerfile found!");
+                    }
+
+                    // Get Docker commands out
+                    const parser = require("docker-file-parser");
+                    const options = { includeComments: false };
+                    const commands = parser.parse(dockerFile, options);
+                    const exposeCommands = commands.filter((c: any) => c.name === "EXPOSE");
+
+                    if (exposeCommands.length > 1) {
+                        throw new Error(`Unable to determine port for default ingress. Dockerfile in project ` +
+                            `'${sdmGoal.repo.owner}/${sdmGoal.repo.name}' has more then one EXPOSE instruction: ` +
+                            exposeCommands.map((c: any) => c.args).join(", "));
+                    } else if (exposeCommands.length === 1) {
+                        newTaskDef.family = imageString;
+                        // TODO: Expose the defaults below in client.config.json
+                        newTaskDef.requiresCompatibilities = [ "FARGATE"];
+                        newTaskDef.networkMode = "awsvpc";
+                        newTaskDef.cpu = "256",
+                        newTaskDef.memory = "0.5GB",
+
+                        newTaskDef.containerDefinitions = [
+                            {
+                                name: imageString,
+                                healthCheck: {
+                                    command: [
+                                        "CMD-SHELL",
+                                        `wget -O /dev/null http://localhost:${exposeCommands[0].args[0]} || exit 1`,
+                                    ],
+                                    startPeriod: 30,
+                                },
+                                image: sdmGoal.push.after.image.imageName,
+                                portMappings: [{
+                                    containerPort: exposeCommands[0].args[0],
+                                    hostPort: exposeCommands[0].args[0],
+                                }],
+                            },
+                        ];
+                    }
+                    resolve(newTaskDef);
+                } else {
+
+                    if (inProjectTaskDef !== undefined) {
+                        // Merge in project config onto black definition
+                        newTaskDef = {...newTaskDef, ...inProjectTaskDef};
+                    } else {
+                        newTaskDef = registration.taskDefinition;
+                    }
+
+                    newTaskDef.containerDefinitions.forEach( k => {
+                        if (imageString === k.name) {
+                            k.image = sdmGoal.push.after.image.imageName;
+                        }
+                        // TODO: Expose the defaults below in client.config.json
+                        k.memory = k.hasOwnProperty("memory") && k.memory ? k.memory : 1024;
+                        k.cpu = k.hasOwnProperty("cpu") && k.cpu ? k.cpu : 256;
+                    });
+                    resolve(newTaskDef);
+                }
+
+            } catch (error) {
+                logger.error(error);
+                reject(error);
+            }
+        });
+    }
